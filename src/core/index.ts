d3ecdef5a4eed7424c370c09a3deda0d3149242d
Oleksandr Holovchenko <algolj@macpaw.com>
2026-03-03 14:03:34 +0100

    feat: consolidate exports in core index file for improved module accessibility

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,8 @@
-export {};
+export * from './types';
+export * from './errors';
+export * from './config';
+export * from './retry';
+export * from './sse';
+export * from './validation';
+export { runRequest, setDefaultTransport } from './request';
+export { createStreamTextResult, createStreamResponseResult } from './stream-result';
